f5f44b11390b78af9ac51f6442323acbafa497db
KHUN <khunsoemoe.aung@kbzbank.com>
2022-07-11 16:14:59 +0630

    Initialize Project

--- app/src/main/java/com/demo/findjob/MainActivity.kt
+++ app/src/main/java/com/demo/findjob/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //Ref: https://github.com/gamalragab21/FinderJob
+//https://androidrepo.com/repo/gamalragab21-FinderJob-android-app
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
