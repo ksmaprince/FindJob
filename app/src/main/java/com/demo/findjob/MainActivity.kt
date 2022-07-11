c1da620b93dce94837253ff9ca474318b38edef5
KHUN <khunsoemoe.aung@kbzbank.com>
2022-07-11 16:14:06 +0630

    Initialize Project

---
+++ app/src/main/java/com/demo/findjob/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.demo.findjob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//Ref: https://github.com/gamalragab21/FinderJob
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
